7807b467e3b2ff816ecea4661a744a05885cd6e5
龚闻文 <2019011291@secoder.net>
2021-08-20 15:08:02 +0800

    naive login completed

--- app/src/main/java/com/fairychild/edukguser/LoginActivity.java
+++ app/src/main/java/com/fairychild/edukguser/LoginActivity.java
@@ -2,33 +2,25 @@ package com.fairychild.edukguser;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.DownloadManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
-
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
-import java.net.URL;
-
-import okhttp3.MediaType;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
 
 public class LoginActivity extends AppCompatActivity {
 
     private Button btnSignIn;
+    private EditText etPhone;
+    private EditText etPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         btnSignIn = findViewById(R.id.btn_sign_in);
+        etPhone = findViewById(R.id.login_phone);
+        etPassword = findViewById(R.id.login_password);
         btnSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -36,21 +28,17 @@ public class LoginActivity extends AppCompatActivity {
                     @Override
                     public void run() {
                         try {
-                            URL url = new URL("47.93.101.225:8080/login");
+                            String url = "47.93.101.225:8080/login";
                             String json = "{\n" +
-                                    " \"account\":\"11111111\",\n" +
-                                    " \"password\":\"123456\"\n" +
+                                    " \"account\":\"" + etPhone.getText() + "\",\n" +
+                                    " \"password\":\"" + etPassword.getText() + "\"\n" +
                                     "}";
-                            OkHttpClient client = new OkHttpClient();
-                            Request request = new Request.Builder()
-                                    .url(url)
-                                    .post(RequestBody.create(MediaType.parse("application/json"), json))
-                                    .build();
-                            Response response = client.newCall(request).execute();
+                            String response = OkHttp.post(url, json);
+
                             runOnUiThread(new Runnable() {
                                 @Override
                                 public void run() {
-                                    Toast.makeText(LoginActivity.this, response.message(), Toast.LENGTH_SHORT).show();
+                                    Toast.makeText(LoginActivity.this, response, Toast.LENGTH_SHORT).show();
                                 }
                             });
                         } catch (Exception e) {
---
+++ app/src/main/java/com/fairychild/edukguser/OkHttp.java
@@ -0,0 +1,35 @@
+package com.fairychild.edukguser;
+
+import java.io.IOException;
+
+import okhttp3.*;
+
+public class OkHttp {
+    public static final MediaType JSON
+            = MediaType.get("application/json; charset=utf-8");
+
+    public static String post(String url, String json) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+        RequestBody body = RequestBody.create(JSON, json);
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        }
+    }
+
+    String run(String url) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        }
+    }
+
+
+}
