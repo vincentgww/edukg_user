8c9c8e028e366920617050a12d91cda2c6796bc4
傅唯宁 <2019011282@secoder.net>
2021-08-24 16:03:46 +0800

    Add Bottom Navigation Bar

--- app/src/main/java/com/fairychild/edukguser/MainActivity.java
+++ app/src/main/java/com/fairychild/edukguser/MainActivity.java
@@ -1,27 +1,40 @@
 package com.fairychild.edukguser;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
-    private Button btnMe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnMe = findViewById(R.id.btn_me);
-        btnMe.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // 跳转到个人界面
-                Intent intent = new Intent(MainActivity.this, MeActivity.class);
-                startActivity(intent);
-            }
-        });
+        BottomNavigationView navigation=(BottomNavigationView) findViewById(R.id.m_bottom_navigation);
+        navigation.setOnNavigationItemReselectedListener((BottomNavigationView.OnNavigationItemReselectedListener) mOnNavigationItemSelectedListener);
     }
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener=new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()){
+                case R.id.navigation_home:
+                    return true;
+                case R.id.navigation_query:
+                    return true;
+                case R.id.navigation_functions:
+                    return true;
+                case R.id.navigation_me:
+                    Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                    startActivity(intent);
+            }
+            return false;
+        }
+    };
 }
