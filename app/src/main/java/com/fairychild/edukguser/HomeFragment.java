11fd7193fd8ea2b86654483d1cc0e3a3d05e85a6
傅唯宁 <2019011282@secoder.net>
2021-08-26 00:12:57 +0800

    Modified to tabs

--- app/src/main/java/com/fairychild/edukguser/HomeFragment.java
+++ app/src/main/java/com/fairychild/edukguser/HomeFragment.java
@@ -11,6 +11,8 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class HomeFragment extends Fragment {
     public static HomeFragment newInstance(){
         HomeFragment indexFragment = new HomeFragment();
@@ -28,6 +30,24 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home,container,false);
+        Activity mActivity=getActivity();
+//        TabLayout layout=mActivity.findViewById(R.id.tab_layout);
+//        layout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+//            @Override
+//            public void onTabSelected(TabLayout.Tab tab) {
+//
+//            }
+//
+//            @Override
+//            public void onTabUnselected(TabLayout.Tab tab) {
+//
+//            }
+//
+//            @Override
+//            public void onTabReselected(TabLayout.Tab tab) {
+//
+//            }
+//        });
         return view;
     }
 }
